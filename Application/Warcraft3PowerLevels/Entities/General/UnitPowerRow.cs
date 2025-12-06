9b74076ae363a9a86cadf9c02dad4981013f6c27
SSchwarberg <S_Schwarberg@web.de>
2025-12-06 20:36:08 +0100

    Added a units power page.

---
+++ Application/Warcraft3PowerLevels/Entities/General/UnitPowerRow.cs
@@ -0,0 +1,16 @@
+﻿namespace Warcraft3PowerLevels.Entities.General
+{
+    /// <summary>
+    /// Represents a data row containing power level information for a unit.
+    /// </summary>
+    /// <param name="Unit"></param>
+    /// <param name="Dps"></param>
+    /// <param name="EffectiveHp"></param>
+    /// <param name="Power"></param>
+    public sealed record UnitPowerRow(
+        IUnit Unit,
+        double Dps,
+        int EffectiveHp,
+        double Power);
+
+}
---
+++ Application/Warcraft3PowerLevels/Services/PowerCalculator.cs
@@ -0,0 +1,49 @@
+﻿using Warcraft3PowerLevels.Entities.General;
+
+namespace Warcraft3PowerLevels.Services;
+
+/// <summary>
+/// Provides methods to calculate the power level of units.
+/// </summary>
+public static class PowerCalculator
+{
+    private const double DummyDps = 25;       // Dummy deals 50 DPS
+    private const double DummyArmor = 10;     // Dummy has 10 armor
+
+    private static double ArmorDamageMultiplier(double armor)
+    {
+        if (armor >= 0)
+        {
+            double reduction = (armor * 0.06) / (1.0 + 0.06 * armor);
+            return 1.0 - reduction;
+        }
+        else
+        {
+            return 2.0 - Math.Pow(0.94, -armor);
+        }
+    }
+
+    public static double ComputeDps(IUnit unit) =>
+        unit.Attack / unit.AttackTime;
+
+    public static int ComputeEffectiveHp(IUnit unit)
+    {
+        double mult = ArmorDamageMultiplier(unit.Armor);
+        return (int)(unit.Health / mult);
+    }
+
+    public static double ComputePower(IUnit unit)
+    {
+        double unitDps = ComputeDps(unit);
+
+        // DPS vs Dummy = apply Dummy armor
+        double unitDpsVsDummy = unitDps * ArmorDamageMultiplier(DummyArmor);
+
+        // Dummy DPS vs Unit = apply Unit armor
+        double dummyDpsVsUnit = DummyDps * ArmorDamageMultiplier(unit.Armor);
+
+        double timeToDie = unit.Health / dummyDpsVsUnit;
+
+        return unitDpsVsDummy * timeToDie;
+    }
+}
