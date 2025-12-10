a1468f53fdb82d4d7806be4590254d10065eded6
SSchwarberg <S_Schwarberg@web.de>
2025-12-10 16:20:45 +0100

    Added a Version control.

---
+++ Application/Warcraft3PowerLevels/Entities/General/VersionConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Warcraft3PowerLevels.Entities.General
+{
+    public static class VersionConstants
+    {
+        public const string CurrentVersion = "1.0.0.0";
+        public const string Wc3Version = "2.0.3.23175";
+    }
+}
